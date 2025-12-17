45d69ba27cae3852d9ab3421a5ecada74488c9fc
Ryan Stenersen <143983140+ryansten20@users.noreply.github.com>
2025-12-16 21:13:51 -0800

    vercel hosting

--- next.config.js
+++ next.config.js
@@ -3,12 +3,7 @@ const onGithubActions = process.env.GITHUB_ACTIONS === 'true'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'export',
-  trailingSlash: true,
-  images: { unoptimized: true },
-
-  ...(onGithubActions ? { basePath: `/${repo}`, assetPrefix: `/${repo}/` } : {}),
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
