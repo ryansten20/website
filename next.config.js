f43b6c436102fb6cfc171741519119e50acfe317
Ryan Stenersen <143983140+ryansten20@users.noreply.github.com>
2025-12-16 20:18:25 -0800

    config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,14 @@
-/** @type {import('next').NextConfig} */
-const repo = 'website' // <-- change this
+const repo = 'website'
+const onGithubActions = process.env.GITHUB_ACTIONS === 'true'
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   output: 'export',
   trailingSlash: true,
+  images: { unoptimized: true },
 
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
-
-  images: {
-    unoptimized: true,
-  },
+  ...(onGithubActions ? { basePath: `/${repo}`, assetPrefix: `/${repo}/` } : {}),
 }
 
 module.exports = nextConfig
-
